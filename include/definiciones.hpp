b90187d6992592dccd6f2c1c725eab770ac13eaa
AntoCreed777 <abenavides@desem.cl>
2025-05-25 18:15:34 -0400

    chore: Se agregan la definicion de Colores

--- include/definiciones.hpp
+++ include/definiciones.hpp
@@ -27,6 +27,16 @@ typedef std::pair<int, int> par;
 #define INF std::numeric_limits<int>::max()
 #define NEG_INF std::numeric_limits<int>::min()
 
+//Constantes para manejar colores
+#define ROJO        "\033[0;31m"
+#define VERDE       "\033[0;32m"
+#define AMARILLO    "\033[0;33m"
+#define AZUL        "\033[0;34m"
+#define MAGENTA     "\033[1;35m"
+#define CIAN        "\033[1;36m"
+#define BLANCO      "\033[1;37m"
+#define RESET_COLOR "\033[0m"
+
 /**
  * @brief Función para optimizar la entrada/salida
  * 
